5220e931d7efc1dd3af8814a29d9f0b95b46faa9
Andrian-alt <andrian.husak@gmail.com>
2020-03-04 13:56:07 +0200

    Updates

---
+++ precache-manifest.540fc7f010f5dc895e0c45c4d3da26fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd02e566534d54819a74a7714b3b871",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "43edfc4a3b8f74dfda52",
+    "url": "/weather/static/css/main.f148f30f.chunk.css"
+  },
+  {
+    "revision": "681740fc0ab32a13f6ce",
+    "url": "/weather/static/js/2.e115a758.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.e115a758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43edfc4a3b8f74dfda52",
+    "url": "/weather/static/js/main.ada355d4.chunk.js"
+  },
+  {
+    "revision": "5b17bd98cdf810bea1eb",
+    "url": "/weather/static/js/runtime-main.2933ce9b.js"
+  },
+  {
+    "revision": "3f16540cd7be3e81eb86de17f1beb51c",
+    "url": "/weather/static/media/cloudly.3f16540c.jpg"
+  },
+  {
+    "revision": "29d4e54fb0f55bfb0648c073de17ebeb",
+    "url": "/weather/static/media/main.29d4e54f.jpg"
+  },
+  {
+    "revision": "7ddd3a418fe03185fee76cf5a2e9db67",
+    "url": "/weather/static/media/rainy.7ddd3a41.jpg"
+  },
+  {
+    "revision": "ebcbf77baa4592e9fd1d80ef6ec8193d",
+    "url": "/weather/static/media/snow.ebcbf77b.jpg"
+  }
+]);
